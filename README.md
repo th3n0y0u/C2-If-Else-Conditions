a95c3a8534898e2d1972a3975902629b0920b31d
Do Bryan <110252@eesd.org>
2021-01-26 00:37:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## If/Elif/Else Conditional Statements
+Used for decision-making operations with conditions
+
+Conditional statements always start with an
+`if` header
+`elif` optional, but we can have as many as we want
+`else` optional, but only 1 for every `if`
+
+### Condition:
+An expression that evaluates to produce a result which is a Boolean value (AKA. Boolean expression) 
+
+### Indentation:
+Python relies on indentation to define scope in the code
+
+### Formula:
+if (Condition):
+> Body Statement
+
+elif (Condition):
+> Body Statement
+
+else (Condition):
+> Body Statement
+
+*Ex. *
+```
+
+a = 2
+
+if (a == 0):
+ print("hi")
+else:
+ print("bye")
+```
+
+> bye
---
+++ main.py
@@ -0,0 +1,38 @@
+a = 200
+b = 33
+
+if(b > a):
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# Write a conditional statement that evaluates if the user input is positive or negative
+number = input("Please input a number: ")
+if (int(number) > 0):
+  print(number + " is a positive number.")
+elif(int(number) == 0):
+  print("You entered 0")
+else:
+  print(number + " is a negative number")
+
+# Ask the user for their age
+# If they are younger than 13, tell them they can only watch PG/G Movies|
+# If they are 13 and older but younger than 17, they can only watch PG-13/PG/G movies
+# If they are 17 and older, they can watch all movies
+
+age = int(input("Please input your age here: "))
+if(age < 13):
+  print("You can only watch PG/G rated movies. ")
+elif(age < 17):
+  print("You can only watch PG-13/PG/G rated movies")
+else:
+  print("You can watch any movies")
+
+  is_Hungry = True
+  is_Sleepy = False
+if(is_Hungry == True):
+ print("You should go eat")
+elif(is_Sleepy == True):
+ print("You should go to sleep")
+elif(is_Sleepy == False):
+  print("Wow you're well-rested")
